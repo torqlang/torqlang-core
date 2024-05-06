e4120e01635bc61a0abe9b7166154cc09be99e8d
glennosborne <glenn@theosbornes.org>
2024-05-06 07:40:43 -0400

    Merge actor package into local

--- core-local/src/main/java/module-info.java
+++ core-local/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module org.torqlang.core.local {
 
-    requires org.torqlang.core.actor;
     requires org.torqlang.core.lang;
     requires org.torqlang.core.klvm;
     requires org.torqlang.core.util;
--- examples/src/main/java/module-info.java
+++ examples/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module org.torqlang.examples {
 
-    requires org.torqlang.core.actor;
     requires org.torqlang.core.klvm;
     requires org.torqlang.core.lang;
     requires org.torqlang.core.local;
